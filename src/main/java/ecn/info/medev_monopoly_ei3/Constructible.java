7cac4ba752ad56a6533ae49594dc95079dc4092c
paulremondeau <poweraider91@gmail.com>
2021-11-18 10:19:01 +0100

    Classe Constructible

--- src/main/java/ecn/info/medev_monopoly_ei3/Constructible.java
+++ src/main/java/ecn/info/medev_monopoly_ei3/Constructible.java
@@ -4,10 +4,64 @@
  */
 package ecn.info.medev_monopoly_ei3;
 
+
+import org.junit.Test;
+
 /**
  *
  * @author bodet
  */
-public class Constructible extends Achetable{
+public class Constructible extends Achetable {
+
+    static public int a;
+    static public int b;
     
+    private int nbMaison ;
+    private int nbHotel ;
+
+    
+    public int calculLoyer() {
+        
+        int res;
+        
+        res += nbMaison*a + nbHotel*b;
+        return res;
+        
+    }
+
+    public String toString() {
+
+        String res = super.toString();
+
+        if (nbMaison > 0) {
+
+            res += ", ";
+            if (nbMaison == 1) {  // Pour gérer le singulier
+
+                res += "1 maison, ";
+
+            } else {
+
+                res += nbMaison + " maisons, ";
+                
+            }
+        }
+
+        if (nbHotel > 0) {
+
+            if (nbHotel == 1) {  // Pour gérer le singulier
+
+                res += "1 hotel,";
+
+            } else {
+
+                res += nbHotel + " hotels,";
+                
+            }
+        }
+
+        res += " loyer = " + calculLoyer() + "€"
+        return res;
+    }
+
 }

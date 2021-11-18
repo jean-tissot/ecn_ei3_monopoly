3afc94437c9df25eccca53fccebfd51f8ec4b6ac
UlrickBL <u.ble@orange.fr>
2021-11-18 09:41:03 +0100

    ajout des sous classes non abstraites

---
+++ src/main/java/ecn/info/medev_monopoly_ei3/Depart.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.info.medev_monopoly_ei3;
+
+/**
+ *
+ * @author uble
+ */
+public class Depart extends NonAchetable{
+    public String toString(){
+        String message = new String();
+        return(message);
+    }
+}

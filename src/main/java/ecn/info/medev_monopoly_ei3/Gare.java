cfec2a20c1492cadb834fdb50bd63d3ad75ddcc8
AlexandreBodet <bodetalexandre1999@gmail.com>
2021-11-18 09:47:00 +0100

    dummycommit

---
+++ src/main/java/ecn/info/medev_monopoly_ei3/Gare.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecn.info.medev_monopoly_ei3;
+
+/**
+ *
+ * @author bodet
+ */
+public class Gare extends Achetable {
+    
+}

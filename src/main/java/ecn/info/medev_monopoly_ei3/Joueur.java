d7a7edc17f81d09eebc9a8cdb0c924258a1aaee5
Noé <noemasson@laposte.net>
2021-11-18 09:25:56 +0100

    Added getters / setters

--- src/main/java/ecn/info/medev_monopoly_ei3/Joueur.java
+++ src/main/java/ecn/info/medev_monopoly_ei3/Joueur.java
@@ -12,6 +12,48 @@ package ecn.info.medev_monopoly_ei3;
  * @author Feibiao Wu
  */
 public class Joueur {
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the fortune
+     */
+    public int getFortune() {
+        return fortune;
+    }
+
+    /**
+     * @param fortune the fortune to set
+     */
+    public void setFortune(int fortune) {
+        this.fortune = fortune;
+    }
+
+    /**
+     * @return the position
+     */
+    public Case getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(Case position) {
+        this.position = position;
+    }
     
     private String nom ;
     private int fortune ;
@@ -21,7 +63,7 @@ public class Joueur {
     public Joueur(String nom, Plateau plateau)
     {
         this.nom = nom ;
-        this.forture = 100000 ;
+        this.fortune = 100000 ;
         this.position  = new Depart();
         this.plateau = plateau ;
     }

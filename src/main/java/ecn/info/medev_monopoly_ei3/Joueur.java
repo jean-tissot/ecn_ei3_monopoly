cdd07567f787152e6f63919ab08ac26495f3641e
Valentin <valentin@molina.pro>
2021-11-18 09:14:47 +0100

    create joueur class

---
+++ src/main/java/ecn/info/medev_monopoly_ei3/Joueur.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.info.medev_monopoly_ei3;
+
+/**
+ *
+ * @author Valentin Molina
+ * @author Noé Masson
+ * @author Feibiao Wu
+ */
+public class Joueur {
+    
+}

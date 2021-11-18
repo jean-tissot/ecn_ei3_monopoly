dce331848b99737e21aad1eddf550d28ab57d35d
UlrickBL <u.ble@orange.fr>
2021-11-18 09:29:06 +0100

    class nonAchetable

---
+++ src/main/java/ecn/info/medev_monopoly_ei3/nonAchetable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecn.info.medev_monopoly_ei3;
+
+/**
+ *
+ * @author uble
+ */
+public class nonAchetable {
+    
+}
